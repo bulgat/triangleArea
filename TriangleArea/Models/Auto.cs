df448c6b4b53623cf9a805fd1e2b94fcfb4b7ff1
Nikolay K <sirbulgat@gmail.com>
2023-10-23 22:52:04 +0300

    Squashed commit of the following:
    commit 775d056568c9b8a93fe9a5abb236674266f1a00f
    Author: kol <sirbulgat@gmail.com>
    Date:   Fri Mar 24 13:43:36 2023 +0300
    
        protected
    
    commit 5cd2b5431849696b7529967b423e79d829888f3e
    Author: kol <sirbulgat@gmail.com>
    Date:   Fri Mar 24 13:41:27 2023 +0300
    
        y
    
    commit 54ce08f9c0efe28806ecc5e989643b53e74fb278
    Author: kol <sirbulgat@gmail.com>
    Date:   Fri Mar 24 13:40:35 2023 +0300
    
        remove garbage

---
+++ TriangleArea/Models/Auto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TriangleArea.Models
+{
+    public class Auto : ITransport
+    {
+        protected string Name= "Mers";
+
+        public Auto()
+        {
+            this.Name = "Super Mers";
+        }
+        public void Drive()
+        {
+            System.Diagnostics.Debug.WriteLine("--Машина едет по дороге");
+        }
+    }
+}
