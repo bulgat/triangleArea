0305881aaa7b0e62b62a8b1ef612c54e21f667aa
bulgat <sirbulgat@gmail.com>
2023-11-29 21:50:03 +0300

    change one1111
    Change adapter Programm

--- TriangleArea/Models/AdapterProgram.cs
+++ TriangleArea/Models/AdapterProgram.cs
@@ -37,10 +37,8 @@ namespace TriangleArea.Models
 
         public AdapterProgram()
         {
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
+            
+           // System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
             ThreadWait.Main();
             ThreadMutex.Main();
             ThreadSemaphore.Main();
@@ -68,15 +66,15 @@ namespace TriangleArea.Models
                     //writer.Flush();
                 }
             });
-            System.Diagnostics.Debug.WriteLine("=0005 " + DateTime.Now.Millisecond);
+           // System.Diagnostics.Debug.WriteLine("=0005 " + DateTime.Now.Millisecond);
 
  
             System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
-            System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
+            //System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
+            //System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
+            //System.Diagnostics.Debug.WriteLine("=0006 " + DateTime.Now.Millisecond);
 
-            System.Diagnostics.Debug.WriteLine("0009 " );
+          //System.Diagnostics.Debug.WriteLine("0009 " );
 
             Stack<int> stackList = new Stack<int>();
             stackList.Push(1);
